d2de3d031e8e825d8ad787b38a194c897093cddb
munkhmngl <munkhmngl@gmail.com>
2023-09-11 22:30:01 +0800

    компьютер жаахан асуудал үүслээ

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,25 @@
 import Search from './model/search';
+// Web app төлөв
+// Хайлтын quety, үр дүн
+// Тухайн үзүүлж байгаа жор
+// Лайкласан жорууд
+// Захиалж байгаа жорын найрлаганууд
 
-let search = new Search('pasta');
-search.doSearch().then(r => console.log(r));
+const controlSearch = () => {
+   // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна
+   const query = document.querySelector('.search__field');
+   if(query){
+      // 2) шинээр хайлтын обьектийг үүсгэж өгнө
+      console.log(query.value);
+      query.value = '';
+      // 3) хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ
+      // 4) хайлтыг гүйцэтгэнэ
+      // 5) хайлтын үр дүнг дэлгэцэнд үзүүлнэ
+   }
+   console.log(`btn click`);
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+   e.preventDefault();
+   controlSearch();
+});
